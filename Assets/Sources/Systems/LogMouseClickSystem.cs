387d52f46af65b014aeab17bb72499f304e581c5
et9930 <815077688@qq.com>
2018-11-17 02:57:14 +0800

    add Log Mouse Clicks

---
+++ Assets/Sources/Systems/LogMouseClickSystem.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------
+// Solution: ECS-Game
+// File name: LogMouseClickSystem.cs
+// -----------------------------
+// Author: 王超
+// Create in: 2018-11-17 2:52
+// -----------------------------
+// Modifier: 王超
+// Modify in: 2018-11-17 2:53
+// -----------------------------
+using Entitas;
+using UnityEngine;
+
+public class LogMouseClickSystem : IExecuteSystem
+{
+    readonly GameContext _context;
+
+    public LogMouseClickSystem(Contexts contexts)
+    {
+        _context = contexts.game;
+    }
+
+    public void Execute()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _context.CreateEntity().AddDebugMessage("Left Mouse Button Clicked");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            _context.CreateEntity().AddDebugMessage("Right Mouse Button Clicked");
+        }
+    }
+}
--- Assets/Sources/Systems/TutorialSystems.cs
+++ Assets/Sources/Systems/TutorialSystems.cs
@@ -15,6 +15,8 @@ public class TutorialSystems : Feature
     public TutorialSystems(Contexts contexts) : base("Tutorial Systems")
     {
         Add(new HelloWorldSystem(contexts));
+        Add(new LogMouseClickSystem(contexts)); // new system
         Add(new DebugMessageSystem(contexts));
+        Add(new CleanupDebugMessageSystem(contexts)); // new system (we want this to run last)
     }
 }
